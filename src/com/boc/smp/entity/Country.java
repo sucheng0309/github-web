47c130a8f0e2dc2347ddd3c242b2bb5159fb0c7d
sucheng <sucheng2015@163.com>
2016-04-09 18:19:37 +0800

    Country

---
+++ src/com/boc/smp/entity/Country.java
@@ -0,0 +1,19 @@
+package com.boc.smp.entity;
+
+public class Country {
+	
+	private String country = "Country";
+	
+	public String getCountry(){
+		return country;
+	}
+
+	
+
+	public void setCountry(String name) {
+		this.country = name;
+	}
+	
+	
+
+}
